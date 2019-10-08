4b6d67863e9a5a479a2a5efbbb6b808fc1fcb07b
nbuikema <46764298+nbuikema@users.noreply.github.com>
2019-10-08 15:01:01 -0500

    added image gallery to projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -450,9 +450,32 @@ window.onload = function() {
 		  new TxtType(elements[i], JSON.parse(toRotate), period);
 		}
 	}
-	// INJECT CSS
 	var css = document.createElement("style");
 	css.type = "text/css";
 	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
 	document.body.appendChild(css);
 };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  	showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  	showSlides(slideIndex = n);
+}
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+}
