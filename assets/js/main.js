d8a0fc195fd4c668698f0acb3d61366c92bafed5
Nick Buikema <nick.buikema@gmail.com>
2019-10-13 19:17:49 -0500

    improved typewriter readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,10 +151,10 @@ TxtType.prototype.tick = function() {
 	var i = this.loopNum % this.toRotate.length;
 	var fullTxt = this.toRotate[i];
 
-	if (this.isDeleting) {
-	this.txt = fullTxt.substring(0, this.txt.length - 1);
+	if(this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
 	} else {
-	this.txt = fullTxt.substring(0, this.txt.length + 1);
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
 	}
 
 	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
@@ -162,19 +162,19 @@ TxtType.prototype.tick = function() {
 	var that = this;
 	var delta = 200 - Math.random() * 100;
 
-	if (this.isDeleting) { delta /= 2; }
+	if(this.isDeleting) { delta /= 2; }
 
-	if (!this.isDeleting && this.txt === fullTxt) {
-	delta = this.period;
-	this.isDeleting = true;
-	} else if (this.isDeleting && this.txt === '') {
-	this.isDeleting = false;
-	this.loopNum++;
-	delta = 500;
+	if(!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if(this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
 	}
 
 	setTimeout(function() {
-	that.tick();
+		that.tick();
 	}, delta);
 };
 
@@ -184,11 +184,11 @@ window.onload = function() {
 		var toRotate = elements[i].getAttribute('data-type');
 		var period = elements[i].getAttribute('data-period');
 		if (toRotate) {
-		  new TxtType(elements[i], JSON.parse(toRotate), period);
+		  	new TxtType(elements[i], JSON.parse(toRotate), period);
 		}
 	}
 	var css = document.createElement("style");
 	css.type = "text/css";
-	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff; animation: blinkTextCursor 500ms steps(1000) infinite normal; }";
 	document.body.appendChild(css);
 };
